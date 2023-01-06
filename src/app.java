8ff853ea869cf59f49347ef55e0a02cb536ef3dd
Joao Pedro <jpcar200@gmail.com>
2023-01-06 19:14:11 -0300

    update app

--- src/app.java
+++ src/app.java
@@ -7,7 +7,7 @@ import server.RmiServer;
 public class app {
     public static void main(String[] args) {
         try {
-            RmiServer server1 = new RmiServer(3232, 0, "sistemasdistribuido");
+            RmiServer server1 = new RmiServer(3232, 0, "sistemasdistribuidos");
             server1.addNewMember(new RmiServer(3333, 1, "sistemasdistribuidos2"));
             server1.addNewMember(new RmiServer(3434, 2, "sistemasdistribuidos3"));
         } catch (RemoteException e) {
